ccc34a6c8266678e7b1956e41e90abafe6f3370f
Upek Hansaja <upekhansajabk@gmail.com>
2025-07-25 11:42:45 +0530

    Composite - practice Q Main

--- structural/composite/practice-question/java/EditDocsApp.java
+++ structural/composite/practice-question/java/EditDocsApp.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 interface DocumentElements {
 
@@ -37,10 +39,62 @@ class TextBox implements DocumentElements {
     }
 }
 
+class Page implements DocumentElements {
+    private String name;
+    private List<DocumentElements> elements = new ArrayList<>();
+
+    public Page(String name) {
+        this.name = name;
+    }
+
+    public void addElement(DocumentElements element) {
+        elements.add(element);
+    }
+
+    public void removeElement(DocumentElements element) {
+        elements.remove(element);
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Rendering page: " + name);
+        for (DocumentElements element : elements) {
+            element.render();
+        }
+    }
+
+    @Override
+    public void move(int x, int y) {
+        System.out.println("Moving page: " + name + " to (" + x + ", " + y + ")");
+        for (DocumentElements element : elements) {
+            element.move(x, y);
+        }
+    }
+
+    @Override
+    public void resize(double factor) {
+        System.out.println("Resizing page: " + name + " by factor " + factor);
+        for (DocumentElements element : elements) {
+            element.resize(factor);
+        }
+    }
+}
+
 public class EditDocsApp {
 
     public static void main(String[] args) {
         
+        DocumentElements textBox1 = new TextBox(10, 20, 1.5);
+        DocumentElements textBox2 = new TextBox(30, 40, 2.0);
+
+        Page page1 = new Page("Introduction");
+        page1.addElement(textBox1);
+        page1.addElement(textBox2);
+
+        page1.render();
+        page1.move(5, 10);
+        page1.resize(1.2);
+
     }
     
 }
