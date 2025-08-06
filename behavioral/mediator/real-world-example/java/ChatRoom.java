8323312b87290b92bab85c549636dddf12803ae9
Upek Hansaja <upekhansajabk@gmail.com>
2025-08-06 12:19:28 +0530

    real-world-example mediator

--- behavioral/mediator/real-world-example/java/ChatRoom.java
+++ behavioral/mediator/real-world-example/java/ChatRoom.java
@@ -1,13 +1,54 @@
-class User{
-    private String name;
-    
-    public User(String name) {
-        this.name = name;
+import java.util.List;
+import java.util.ArrayList;
+
+interface Mediator {
+    void sendMessage(String message, User sender);
+    void addUser(User user);
+}
+
+class ChatRoomMediator implements Mediator {
+
+    private List<User> users;
+
+    public ChatRoomMediator() {
+        this.users = new ArrayList<>();
     }
 
-    public void sendMessage(String message, User recipient) {
-        System.out.println(this.name + " sends message: " + message + " to " + recipient.getName());
-        // Here we would normally call a method on the ChatRoom to handle the message
+    @Override
+    public void sendMessage(String message, User sender) {
+        for (User user : users) {
+            if (user != sender) {
+                user.receive(message, sender.getName());
+            }
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+}
+
+class User {
+    private String name;
+    private Mediator mediator;
+
+    public User(String name, Mediator mediator) {
+        this.name = name;
+        this.mediator = mediator;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void send(String message) {
+        mediator.sendMessage(message, this);
+    }
+
+    public void receive(String message, String senderName) {
+        System.out.println(name + " received message from " + senderName + ": " + message);
     }
 }
 
@@ -15,11 +56,20 @@ public class ChatRoom {
 
     public static void main(String[] args) {
         
-        User user1 = new User("Upek");
-        User user2 = new User("Rashi");
+        ChatRoomMediator chatRoom = new ChatRoomMediator();
 
-        user1.sendMessage("Hello, Rashi!", user2);
-        user2.sendMessage("Hi, Upek! How are you?", user1);
+        User user1 = new User("Upek", chatRoom);
+        User user2 = new User("Rashi", chatRoom);
+        User user3 = new User("Senu", chatRoom);
+
+        chatRoom.addUser(user1);
+        chatRoom.addUser(user2);
+        chatRoom.addUser(user3);
+
+        user1.send("Hello everyone!");
+        user2.send("Hi Upek!");
+        user3.send("Good morning Upek and Rashi!");
+        user1.send("Good morning Senu!");
 
     }
     
